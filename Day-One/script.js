86b59d07805658b976ac2957faf39f46620b3de5
Sujata Ghosh <sujata.mandardihi@gmail.com>
2024-01-09 22:11:54 +0530

    first commit

---
+++ Day-One/script.js
@@ -0,0 +1,55 @@
+// Variable var, let, const
+
+/*var a = 'Mona, solly ';
+var a = "Mona I Love You";
+console.log(a);*/
+
+
+
+// let a = "I love javascript";
+
+// // let a = "Love"; error
+// a = "sss";
+// console.log(a);
+
+// const a = "Javascript";
+
+// a = "jhjhjh";//I dont change the value
+// console.log(a);
+
+// ////// Data Type //////
+// String, Symbol, Boolean, BigInt, Null, Number, Undefine => Primitive
+// Object, Array =>Non-Primitive //array er typeof korle object hisabe dekhai
+// const str="String";
+// const Number=20;
+// const bool=true;
+// const falseValue=null;
+// const arr=["1", "2"];
+// const obj={firstname: "Sujata"};
+
+// //Typeof Operator
+// console.log(typeof arr);
+
+// console.log(2 ** 3); //2 to the power 3
+
+
+//// Comparison Operator ////
+
+/*//1. Equality Operator (==) Return a boolean
+console.log(3==3);
+console.log(true==true);
+console.log("2"==2);//typeof er jonno etao true return korbe 
+console.log("Hello World"=="Hello World");
+// 2. Inequality Operator (!=)
+console.log(3!=5);
+
+//3. Identity Operator (===), Strictly Equality Operator (ekane typeof khate na)
+console.log(3===2); //false return korbe
+console.log(2===2); //true
+
+//4. Non-Identity Operator(!==), Strictly Inequality Operator
+
+console.log(3!==2); //true
+console.log(3!=='3'); //true
+
+///// Logical Operator*/
