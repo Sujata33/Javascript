195c0d7019dc92e7a50b1371a203fcd40ddf1ff2
Sujata Ghosh <sujata.mandardihi@gmail.com>
2024-01-13 23:20:29 +0530

    Math Object

---
+++ Day-Four/script.js
@@ -0,0 +1,24 @@
+//Math Object
+
+const PI = Math.PI;
+console.log(PI);
+
+const gravity = 9.81;
+
+console.log(Math.ceil(gravity));
+
+console.log(Math.round(gravity));
+
+console.log(Math.floor(gravity));
+
+console.log(Math.max(-5, 3, 20, 8, 4));
+
+console.log(Math.min(-6, 8, 90, 54, 234));
+
+console.log(Math.sqrt(100));
+
+console.log(Math.cbrt(100));
+
+console.log(Math.sin(60));
+
+console.log(Math.pow(3, 2));
