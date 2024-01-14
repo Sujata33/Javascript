6c0eae5b2784285cf3e7f5ae633f567248bab2eb
Sujata Ghosh <sujata.mandardihi@gmail.com>
2024-01-14 14:04:15 +0530

    template literal

--- Day-Four/script.js
+++ Day-Four/script.js
@@ -22,3 +22,17 @@ console.log(Math.cbrt(100));
 console.log(Math.sin(60));
 
 console.log(Math.pow(3, 2));
+// Template literal
+const string = `square root of 100 is ${Math.sqrt(100)}`;
+console.log(string);
+
+const str1 = `${Math.cbrt(27)} is the cube root of 27`;
+console.log(str1);
+
+const lang = "Javascript";
+console.log(`I am learning ${lang}`);
+
+const boy = 2001;
+console.log(`I am ${2024 - boy} years old`);
+
+console.log(boy.toString());
