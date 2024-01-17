dfabdd67d875605e220d937620400b6a97516d52
Sujata Ghosh <sujata.mandardihi@gmail.com>
2024-01-17 23:18:13 +0530

    Level 1 complete and level 2 running

--- Exercises/Data-types/script.js
+++ Exercises/Data-types/script.js
@@ -1,7 +1,7 @@
 ///// Level One////
 
-//1.
 const challenge = "30 Days Of JavaScript";
+/*//1.
 //2.
 console.log(challenge);
 
@@ -36,3 +36,64 @@ console.log(companies.split(", "));
 
 //12.
 console.log(challenge.replace("JavaScript", "Python"));
+
+//13.
+console.log(challenge.charAt(15));
+
+//14. //ASCII
+console.log(challenge.charCodeAt("J"));
+
+//15. 1st index of a
+console.log(challenge.indexOf("a"));
+
+//16. last index of a
+console.log(challenge.lastIndexOf("a"));
+
+//17.
+const sentence = "You cannot end a sentence with because because because is a conjunction";
+console.log(sentence.indexOf("because"));
+
+//18.
+console.log(sentence.lastIndexOf("because"));
+
+//19.
+console.log(sentence.search("because"));
+
+//20.
+console.log(" 30 Days Of JavaScript   ".trim());
+
+//21.
+console.log(challenge.startsWith("30"));
+
+//22.
+console.log(challenge.endsWith("JavaScript"));
+
+//23.
+console.log(challenge.match(/a/g));
+
+//24.
+console.log("30 Days of ".concat("JavaScript"));
+
+//25.
+console.log(challenge.repeat(2));
+*/
+
+///// Level 2///
+
+//1.
+const quote = "The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another.";
+console.log(quote);
+
+//2.
+const quote2 = "Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead.";
+console.log(quote2);
+
+//3.
+const check = "10" === 10 ? true : false;
+console.log(check);
+
+const a = parseInt("10");
+console.log(typeof a);
+
+const check1 = a === 10 ? true : false;
+console.log(check1);
