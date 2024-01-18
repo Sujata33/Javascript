be88b269f40aabada068b7023c0631d9aaff093a
Sujata Ghosh <sujata.mandardihi@gmail.com>
2024-01-18 16:53:33 +0530

    Level 2 complete

--- Exercises/Data-types/script.js
+++ Exercises/Data-types/script.js
@@ -90,10 +90,60 @@ console.log(quote2);
 
 //3.
 const check = "10" === 10 ? true : false;
-console.log(check);
+console.log(check);//false
 
+//make it equal
 const a = parseInt("10");
 console.log(typeof a);
 
 const check1 = a === 10 ? true : false;
 console.log(check1);
+
+//4.
+const b = parseFloat("9.8");
+const check2 = b === 10 ? true : false;
+console.log(check2);//return false
+//Make it equal
+// const check3 = Math.ceil(b) === 10 ? true : false;
+// console.log(check2); Hoche na
+
+//5.
+const check4 = "python".search("on") && "jargon".search("on") ? true : false;
+console.log(check4);
+
+//6.
+const sen = "I hope this course is not full of jargon.";
+console.log(sen.search("jargon") ? true : false);
+
+//7.
+const ran = Math.floor(Math.random() * (100 - 0 + 1) + 0);
+console.log(ran);
+//8.
+const ran1 = Math.floor(Math.random() * (100 - 50 + 1) + 50);
+console.log(ran1);
+//9.
+const ran2 = Math.floor(Math.random() * (255 - 0 + 1) + 0);
+console.log(ran2);
+
+//10.
+
+//11.
+console.log("1 1 1 1 1\n2 1 2 4 8\n3 1 3 9 27\n4 1 4 16 64\n5 1 5 25 125");
+
+//12.hoche na
+const sentence1 = "You cannot end a sentence with because because because is a conjunction";
+
+console.log(sentence1);
+
+////// Level 3///
+
+//1.
+const sentence2 = "Love is the best thing in this world. Some found their love and some are still looking for their love.";
+console.log(sentence2.match(/love/g));
+
+//2.
+console.log(sentence1.match(/because/g));
+
+//3.
+const sentence3 = "%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching";
+console.log(sentence3.replace(/@/g, ""));
