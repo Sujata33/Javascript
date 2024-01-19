f3284f7eadb1e6106ac8eb701cba8b618f0d2295
Sujata Ghosh <sujata.mandardihi@gmail.com>
2024-01-19 22:29:34 +0530

    Math object

--- Day-Three/script.js
+++ Day-Three/script.js
@@ -7,7 +7,7 @@
 const susu = "33";
 console.log(typeof susu);
 
-const ss= parseInt(susu);
+const ss = parseInt(susu);
 console.log(ss);
 console.log(typeof ss);
 
@@ -35,5 +35,44 @@ const str2 = ss.toString();
 console.log(str2);//toString() don't pass argument in this method
 console.log(typeof str2);
 
-console.log(3+5+"2" + 6); //concatinate
+console.log(3 + 5 + "2" + 6); //concatinate
 console.log("9" - 3);// type conversion
+
+//Math Object
+
+const PI = Math.PI;
+console.log(PI);
+
+const gravity = 9.81;
+
+console.log(Math.ceil(gravity));
+
+console.log(Math.round(gravity));
+
+console.log(Math.floor(gravity));
+
+console.log(Math.max(-5, 3, 20, 8, 4));
+
+console.log(Math.min(-6, 8, 90, 54, 234));
+
+console.log(Math.sqrt(100));
+
+console.log(Math.cbrt(100));
+
+console.log(Math.sin(60));
+
+console.log(Math.pow(3, 2));
+// Template literal
+const string = `square root of 100 is ${Math.sqrt(100)}`;
+console.log(string);
+
+const str1 = `${Math.cbrt(27)} is the cube root of 27`;
+console.log(str1);
+
+const lang = "Javascript";
+console.log(`I am learning ${lang}`);
+
+const boy = 2001;
+console.log(`I am ${2024 - boy} years old`);
+
+console.log(boy.toString());
