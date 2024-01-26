4d8a2a2b0e110d6ade57af37a0c08fe65a095a46
Sujata Ghosh <sujata.mandardihi@gmail.com>
2024-01-26 22:57:08 +0530

    Level 1 question no. 3 complete

--- Exercises/Day-3/script.js
+++ Exercises/Day-3/script.js
@@ -1,3 +1,21 @@
 ////Level 1///
 
 //1.
+const firstName = "Sujata";
+const lastName = "Ghosh";
+const country = "India";
+const age = 23;
+const isMarried = false;
+
+console.log(typeof firstName);
+console.log(typeof lastName);
+console.log(typeof country);
+console.log(typeof age);
+console.log(typeof isMarried);
+
+//2.
+console.log(typeof "10" === typeof 10);
+
+//3.
+console.log(parseInt("9.8"));
+console.log(parseInt("9.8") === 10);
