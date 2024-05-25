b4664133f38da31bf80b8d4ed7f362980ae4f41d
Sujata Ghosh <sujata.mandardihi@gmail.com>
2024-05-25 23:26:23 +0530

    level 3 q. 2(a) complete

--- Exercises/object-practice/script.js
+++ Exercises/object-practice/script.js
@@ -124,11 +124,176 @@ copyUsers.sujata = {
 };
 console.log(copyUsers);
 console.log(users);
-*/
 //7.
 for (const country of countries) {
-    console.log(`name: ${country.name}, capital: ${country.capital}, population:${country.population}`);
+    const langArr = country.languages;
+    if (langArr.length === 1) {
+        console.log(`name: ${country.name}, capital: ${country.capital}, population:${country.population}, languages:${langArr.slice(0)}`);
+    } else {
+        console.log(`name: ${country.name}, capital: ${country.capital}, population:${country.population}, languages:${langArr.slice(0, langArr.length - 1)} and ${langArr.slice(langArr.length - 1)}`);
+    }
+
 }
 const langArr = ["a", "c", "d"];
 const fst = langArr.slice(0, 2);
 console.log(fst);
+
+//1.
+const personAccount = {
+    firstName: "Sujata",
+    lastName: "Ghosh",
+    incomes: {
+        salary: 30000,
+        tuitionFees: 2300
+    },
+    expenses: {
+        roomRent: 4500,
+        electricBill: 600,
+        marketBill: 4000,
+        transportationBill: 1000
+    },
+    totalIncome: function () {
+        let sumIncome = 0;
+        for (const income of Object.values(this.incomes)) {
+            sumIncome += income;
+        }
+        return sumIncome;
+    },
+    totalExpense: function () {
+        let sumExpe = 0;
+        for (const exp of Object.values(this.expenses)) {
+            sumExpe += exp;
+        }
+        return sumExpe;
+    },
+    addIncome: function (type, amount) {
+        this.incomes[type] = amount;
+    },
+    addExpense: function (type, amount) {
+        this.expenses[type] = amount;
+    },
+    accountBalance: function () {
+        return this.totalIncome() - this.totalExpense();
+    }
+}
+
+console.log(personAccount.accountBalance());
+personAccount.addIncome("mona", 20000);
+console.log(personAccount);
+
+*/
+const users = [
+    {
+        _id: 'ab12ex',
+        username: 'Alex',
+        email: 'alex@alex.com',
+        password: '123123',
+        createdAt: '08/01/2020 9:00 AM',
+        isLoggedIn: false
+    },
+    {
+        _id: 'fg12cy',
+        username: 'Asab',
+        email: 'asab@asab.com',
+        password: '123456',
+        createdAt: '08/01/2020 9:30 AM',
+        isLoggedIn: true
+    },
+    {
+        _id: 'zwf8md',
+        username: 'Brook',
+        email: 'brook@brook.com',
+        password: '123111',
+        createdAt: '08/01/2020 9:45 AM',
+        isLoggedIn: true
+    },
+    {
+        _id: 'eefamr',
+        username: 'Martha',
+        email: 'martha@martha.com',
+        password: '123222',
+        createdAt: '08/01/2020 9:50 AM',
+        isLoggedIn: false
+    },
+    {
+        _id: 'ghderc',
+        username: 'Thomas',
+        email: 'thomas@thomas.com',
+        password: '123333',
+        createdAt: '08/01/2020 10:00 AM',
+        isLoggedIn: false
+    }
+];
+
+const products = [
+    {
+        _id: 'eedfcf',
+        name: 'mobile phone',
+        description: 'Huawei Honor',
+        price: 200,
+        ratings: [
+            { userId: 'fg12cy', rate: 5 },
+            { userId: 'zwf8md', rate: 4.5 }
+        ],
+        likes: []
+    },
+    {
+        _id: 'aegfal',
+        name: 'Laptop',
+        description: 'MacPro: System Darwin',
+        price: 2500,
+        ratings: [],
+        likes: ['fg12cy']
+    },
+    {
+        _id: 'hedfcg',
+        name: 'TV',
+        description: 'Smart TV:Procaster',
+        price: 400,
+        ratings: [{ userId: 'fg12cy', rate: 5 }],
+        likes: ['fg12cy']
+    }
+]
+
+//2.
+function signUp(newUser, newEmail, password) {
+
+    const idString = "abcdefghijklmnopqrstuvwxyz1234567890";
+    let ranId = "";
+    for (let i = 0; i < 6; i++) {
+        let ranNum = Math.floor(Math.random() * idString.length);
+        ranId += idString[ranNum];
+    }
+
+    const date = new Date();
+    let day = date.getDate() < 9 ? `0${date.getDate()}` : date.getDate();
+    let month = date.getMonth() < 9 ? `0${date.getMonth()}` : date.getMonth();
+    let year = date.getFullYear();
+    let hr = date.getHours() < 9 ? `0${date.getHours()}` : date.getHours();
+    let min = date.getMinutes() < 9 ? `0${date.getMinutes()}` : date.getMinutes();
+    // let sec = date.getSeconds()< 9 ? `0${date.getSeconds()}` : date.getSeconds();
+    let createdAt = `${day}/${month}/${year} ${hr}:${min}`;
+
+    for (const user of users) {
+
+        if (user.username === newUser) {
+            return "you have already an account";
+        }
+
+    }
+    const addUser = {
+        _id: ranId,
+        username: newUser,
+        email: newEmail,
+        password,
+        createdAt,
+        isLoggedIn: false
+
+    }
+    users.push(addUser)
+
+}
+signUp("sujata", "suju@dd.com", "11111");
+// console.log(signUp());
+
+console.log(users);
